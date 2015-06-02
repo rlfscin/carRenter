ecd4438a0283bb945756121356deace47777bebf
thiagocg <thi_grandesso@hotmail.com>
2015-06-02 16:32:30 -0500

    New model classes
    Added new model classes

---
+++ Models/CarRenterContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarRenter.Models
+{
+    public class CarRenterContext : DbContext 
+    {
+        public CarRenterContext() : base()
+        {
+            Database.SetInitializer<CarRenterContext>(new DropCreateDatabaseIfModelChanges<CarRenterContext>());
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        public virtual DbSet<Agency> Agencies { get; set; }
+
+        public virtual DbSet<Rental> Rentals { get; set; }
+    }
+}
---
+++ Models/Rental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRenter.Models
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+
+        public int CarId { get; set; }
+
+        public int CityId { get; set; }
+
+        public DateTime RentDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+    }
+}
